e617bc13785991a229b669ed52e6d5959e898353
rzarxx <rezakusuma1804@gmail.com>
2026-07-14 19:31:44 +0700

    fix(prisma): revert PrismaClient to default constructor and add url back to schema.prisma

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import crypto from "crypto";
 
-const prisma = new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL } } });
+const prisma = new PrismaClient();
 
 export async function POST(request) {
   try {
